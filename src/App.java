472310c41c84dee205b8084d069c2cf75265ac79
IvanaGlez <ivana.gonzalez.patino11@gmail.com>
2023-08-16 18:43:02 -0600

    Project song two

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Mi canción favorita es, Sunflower de Post Malone");
+        System.out.println("Mi canción es, Sunflower de Post Malone");
     }
 }
