a72819ef6a5b18460ca369504dac07c07e07a060
IvanaGlez <ivana.gonzalez.patino11@gmail.com>
2023-08-16 18:56:13 -0600

    Mis canciones favoritas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Mi canción es, Sunflower de Post Malone");
+        System.out.println("Mi canción es,  Beliver de image dragons ");
+        System.out.println("Mi canción es,  Top of the word - score");
+        System.out.println("Mi canción es,  You Belong with Me - Taylor Swift");
+        System.out.println("Mi canción es,  Devuelveme a mi chica de hombres G");
+        System.out.println("Mi canción es,  Titanium de Dvid Guetta");
     }
 }
